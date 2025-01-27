6764e3415a9d549ff305f99f4b70d78c84a64307
EJ6612 <ej_6612@outlook.com>
2025-01-27 12:08:54 -0700

    Week03 has fallen. Go to week4practice.

--- week4practice/angle.h
+++ week4practice/angle.h
@@ -1,58 +1,125 @@
-/*************************************************************
- * 1. Name:
- *      -your name-
- * 2. Assignment Name:
- *      Practice 04: Angle Class
- * 3. Assignment Description:
- *      A class to represent an angle
- * 4. What was the hardest part? Be as specific as possible.
- *      -a paragraph or two about how the assignment went for you-
- * 5. How long did it take for you to complete the assignment?
- *      -total time in hours: reading the assignment, submitting, etc.
- **************************************************************/
+/***********************************************************************
+ * Header File:
+ *    ANGLE
+ * Author:
+ *    Br. Helfrich
+ * Summary:
+ *    Everything we need to know about a direction
+ ************************************************************************/
 
 
+ /* DELETE ME WHEN DONE
+ * Ticket 1: ~45 minutes
+ * Ticket 2: ~60 minutes
+ * Ticket 3: ~45 minutes
+ * Ticket 4: ~45 minutes
+ */
+
+#pragma once
+
+#define _USE_MATH_DEFINES
+#include <math.h>   // for M_PI which is 3.14159
 #define TWO_PI 6.28318530718
 
-#include <math.h>    // for floor()
-#include <iostream>  // for cout
-#include <cassert>   // for assert()
-using namespace std;
-
+class TestPosition;
+class TestVelocity;
+class TestAcceleration;
 class TestAngle;
+class TestLander;
 
- /************************************
-  * ANGLE
-  ************************************/
+/************************************
+ * ANGLE
+ ************************************/
 class Angle
 {
-   friend TestAngle;
-
 public:
-   // Default constructor
-   Angle()                   { }
-   Angle(const Angle & rhs)  { }
-   Angle(double degrees)     { }
+   friend TestAcceleration;
+   friend TestVelocity;
+   friend TestAngle;
+   friend TestLander;
+
+   // Constructors
+   Angle() : radians(0.0) {}
+
+   Angle(const Angle& rhs) : radians(rhs.getRadians()) {}
+
+   Angle(double degrees) : radians(degrees* M_PI / 180) {}
+
 
    // Getters
-   double getDegrees() const { return -1.0; }
-   double getRadians() const { return -1.0; }
+   double getDegrees() const { return radians * (180 / M_PI); }
+   double getRadians() const { return radians; }
 
    // Setters
-   void setDegrees(double degrees) { }
-   void setRadians(double radians) { }
+   void setDegrees(double degrees)
+   {
+      degrees = (degrees * (2 * M_PI)) / 360;
+      radians = normalize(degrees);
+   }
 
-   // Display
-   void display(ostream & out) const { }
+   void setRadians(double aRadian)
+   {
+      radians = normalize(aRadian);
+   }
+
+   void setUp()
+   {
+      // 0°
+      setDegrees(0.0);
+   }
+
+   void setRight()
+   {
+      // 90°
+      setDegrees(90.0);
+   }
+
+   void setLeft()
+   {
+      // 270°
+      setDegrees(270.0);
+   }
+
+   void setDown()
+   {
+      // 180°
+      setDegrees(180.0);
+   }
+
+   void reverse()
+   {
+      radians += M_PI;
+   }
+
+   Angle& add(double delta)
+   {
+      radians += delta;
+
+      // Normalize the angle after addition
+      radians = normalize(radians);
+
+      return *this;
+   }
 
 private:
-   // Convert functions
-   double convertToDegrees(double radians) const { return -1.0; }
-   double convertToRadians(double degrees) const { return -1.0; }
+   double normalize(double aRadian)
+   {
+      // Use fmod to find the remainder of aRadian divided by TWO_PI
+      aRadian = fmod(aRadian, TWO_PI);
 
-   // Normalize
-   double normalize(double radians) const { return  -1.0; }
+      if (aRadian < 0)
+      {
+         aRadian += TWO_PI;
+      }
+      if (aRadian > TWO_PI)
+      {
+         aRadian -= TWO_PI;
+      }
+
+      return aRadian;
+   }
 
    double radians;
+   // 360 degrees equals 2 PI radians
 };
 
