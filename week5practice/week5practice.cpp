38f76faafab69a3796c4b49cf6c6ede33c101c83
EJ6612 <ej_6612@outlook.com>
2025-02-04 16:51:27 -0700

    Stubbed functions.

--- week5practice/week5practice.cpp
+++ week5practice/week5practice.cpp
@@ -4,26 +4,57 @@
 #include <iostream>
 #include <fstream>
 
-void readBalance()
+using namespace std;
+
+/*****************
+* READ BALANCE
+* Returns balance from file.
+******************/
+double readBalance()
 {
-   // return 0.0;
+   // TODO: Pull balance from file.
+   return 0.0;
 }
 
+/*****************
+* UPDATE BALANCE
+* Update balance in file by user input.
+******************/
 void updateBalance()
 {
+   double change;
 
+   std::cout << "Change: ";
+   std::cin >> change;
+
+   // TODO: Update file here.
+   // 
+   // Displaying balance will be handled by main via displayBalance.
 }
 
+/*****************
+* WRITE BALANCE
+* Write a new balance in file.
+******************/
 void writeBalance()
 {
-
+  
 }
 
+/*****************
+* DISPLAY BALANCE
+* Display balance onto the console.
+******************/
 void displayBalance()
 {
+   double balance = readBalance();
 
+   std::cout << "Account balance: $" << balance << endl;
 }
 
+/*****************
+* MAIN
+******************/
 int main()
 {
     std::cout << "Hello World!\n";
