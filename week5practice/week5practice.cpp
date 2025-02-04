9fd56f78dc7be396f4a9a06204cf942d74d52873
EJ6612 <ej_6612@outlook.com>
2025-02-04 16:36:03 -0700

    week 5 practice initialized

---
+++ week5practice/week5practice.cpp
@@ -0,0 +1,30 @@
+// week5practice.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <fstream>
+
+void readBalance()
+{
+   // return 0.0;
+}
+
+void updateBalance()
+{
+
+}
+
+void writeBalance()
+{
+
+}
+
+void displayBalance()
+{
+
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
