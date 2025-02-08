9dc829b01d32c5bb5150cc5a401497d07dd491e6
EJ6612 <ej_6612@outlook.com>
2025-02-08 13:24:45 -0700

    rest function filled, but still at same fail rate of 10%.

--- week5lab/lander.h
+++ week5lab/lander.h
@@ -28,41 +28,41 @@ class Lander
    friend TestLander;
    
 public:
-  // to create a lander, we need to know something about the board
-  Lander(const Position & posUpperRight) : status(DEAD), fuel(-99.0) {  }
+   // to create a lander, we need to know something about the board
+   Lander(const Position & posUpperRight) : status(DEAD), fuel(-99.0) {  }
 
-  // reset the lander and its position
-  void reset(const Position& posUpperRight);
+   // reset the lander and its position
+   void reset(const Position& posUpperRight);
 
-  // get the status of the lander
-  bool     isDead()         const { return true; }
-  bool     isLanded()       const { return true; }
-  bool     isFlying()       const { return true; }
-  Position getPosition()    const { return pos;  }
-  double   getSpeed()       const { return 99.9; }
-  int      getFuel()        const { return -99;  }
-  int      getWidth()       const { return 99;   }
-  double   getMaxSpeed()    const { return 99.9; }
+   // get the status of the lander
+   bool     isDead()         const { return status; }
+   bool     isLanded()       const { return true; }
+   bool     isFlying()       const { return true; }
+   Position getPosition()    const { return pos;  }
+   double   getSpeed()       const { return 99.9; }
+   int      getFuel()        const { return -99;  }
+   int      getWidth()       const { return 99;   }
+   double   getMaxSpeed()    const { return 99.9; }
 
-  // draw the lander on the screen
-  void draw(const Thrust & thrust, ogstream & gout) const;
+   // draw the lander on the screen
+   void draw(const Thrust & thrust, ogstream & gout) const;
  
-  // handle input
-  Acceleration input(const Thrust & thrust, double gravity);
+   // handle input
+   Acceleration input(const Thrust & thrust, double gravity);
 
-  // what happens when we coast?
-  void coast(Acceleration & acceleration, double time);
+   // what happens when we coast?
+   void coast(Acceleration & acceleration, double time);
 
-  // straighten the lander and put it on the ground
-  void land()  {  }
+   // straighten the lander and put it on the ground
+   void land()  {  }
 
-  // we are dead. Draw the lander upside down
-  void crash() {  }
+   // we are dead. Draw the lander upside down
+   void crash() {  }
 
-private:
-   Status   status;      // are we dead or not?
-   Position pos;         // the location of the lander
-   Velocity velocity;    // the speed of the lander
-   Angle    angle;       // the orientation of the lander
-   double   fuel;        // amount of fuel remaining
-};
+   private:
+      Status   status;      // are we dead or not?
+      Position pos;         // the location of the lander
+      Velocity velocity;    // the speed of the lander
+      Angle    angle;       // the orientation of the lander
+      double   fuel;        // amount of fuel remaining
+   };
