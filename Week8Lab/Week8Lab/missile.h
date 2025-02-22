a6caebd3f6cffb371ca6a049e61b727e93c285b7
EJ6612 <ej_6612@outlook.com>
2025-02-22 16:17:20 -0700

    Cleanup

--- Week8Lab/Week8Lab/missile.h
+++ Week8Lab/Week8Lab/missile.h
@@ -4,9 +4,6 @@
 #include "angle.h"
 #include "gravity.h"
 
-class Missile
-{
-public:
 #define MASS 46.70               // kg
 #define INITIAL_VELOCITY 827.00  // m/s
 #define DIAMETER 0.15489         // m
@@ -14,55 +11,59 @@ public:
 #define AREA 0.018842            // m^2
 #define TIME_UNIT 1              // seconds
 
-   Missile() : altitude(0.0), mass(MASS), diameter(DIAMETER), distance(0) {}
-   Missile(double altitude, Acceleration& acc, Velocity& v)
-   {
-      this->altitude = altitude;
-      this->v = v;
-      this->acc = acc;
-   }
+class Missile
+{
+   public:
+
+      Missile() : altitude(0.0), mass(MASS), diameter(DIAMETER), distance(0) {}
+
+      Missile(double altitude, Acceleration& acc, Velocity& v)
+      {
+         this->altitude = altitude;
+         this->v = v;
+         this->acc = acc;
+         this->diameter = DIAMETER;
+         this->mass = MASS;
+         this->distance = 0;
+      }
 
 
-
-private:
-   double altitude;
-   Acceleration acc;
-   Velocity v;
-   double mass;
-   double diameter;
-   double distance;
-   double Altitude;
-
-public:
-	double getAltitude()
-	{
-		return altitude;
-	}
-
-	void setAltitude(double altitude)
-	{
-		this->altitude = altitude;
-	}
-
-	double getDistance() {
-		return distance;
-	}
-
-	void setDistance(double distance) {
-		this->distance = distance;
-	}
-
-   double getArea()
-   {
-      return AREA;
-   }
+   private:
+      double altitude;
+      Acceleration acc;
+      Velocity v;
+      double mass;
+      double diameter;
+      double distance;
 
 
+   public:
 
-   void displayPos(int time)
-   {
-      
-   }
+      // Getters
+	   double getAltitude()
+	   {
+		   return altitude;
+	   }
 
+	   double getDistance() 
+      {
+		   return distance;
+	   }
+
+      double getArea()
+      {
+         return AREA;
+      }
+
+      // Setters
+	   void setAltitude(double altitude)
+	   {
+		   this->altitude = altitude;
+	   }
+
+	   void setDistance(double distance) 
+      {
+		   this->distance = distance;
+	   }
 	
 };
