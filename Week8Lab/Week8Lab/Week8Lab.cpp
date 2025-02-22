059cc4b01d62ca4a59904cfd971a00d2ead3f01d
McClain Lindsay <mj.lindsay@live.com>
2025-02-22 08:16:35 -0800

    lienearInterpolation

--- Week8Lab/Week8Lab/Week8Lab.cpp
+++ Week8Lab/Week8Lab/Week8Lab.cpp
@@ -2,6 +2,9 @@
 //
 
 #include <iostream>
+#include <cmath>
+
+#include <cassert>
 
 
 double calLienearInterpolationDistance(double r1, double r0, double d1, double d0, double r)
@@ -9,15 +12,24 @@ double calLienearInterpolationDistance(double r1, double r0, double d1, double d
     return ((r1 - r0) / ((d1 - d0) * (r - r0))) + d0;
 }
 
-double calLienearInterpolationHeight(double r1, double r0, double d1, double d0, double d)
-{
-	return (r0 + (((r1 - r0) * (d - d0)) / (d1 - d0))); 
+double calLienearInterpolationHeight(double d0, double r0, double d1, double r1, double d) {
+   return (r0 + (((r1 - r0) * (d - d0)) / (d1 - d0)));
 }
 
+void testcalLienearInterpolationHeight() {
+   double tolerance = 1e-4; // Tolerance value for approximation
+   double expected = 1.2024;
+   double result = calLienearInterpolationHeight(0, 1.225, 1000, 1.112, 200);
 
-int main()
-{
-    std::cout << "Hello World!\n";
+   assert(std::abs(result - expected) < tolerance);
+   std::cout << "Test passed! Result is approximately " << expected << std::endl;
+}
+
+int main() {
+   testcalLienearInterpolationHeight();
+   std::cout << "calLienearInterpolationHeight(0, 1.225, 1000, 1.112, 200): " << calLienearInterpolationHeight(0, 1.225, 1000, 1.112, 200) << std::endl;
+
+   return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
