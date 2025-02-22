fced27de2f4e51cec6a6877bd3f4b136b8335557
EJ6612 <ej_6612@outlook.com>
2025-02-22 11:39:11 -0700

    Creation of missile class

---
+++ Week8Lab/Week8Lab/missile.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "velocity.h"
+#include "acceleration.h"
+#include "angle.h"
+
+class Missile
+{
+public:
+   Missile() : altitude(0.0) {}
+   Missile(double altitude, Acceleration& acc, Velocity& v) 
+   {
+      this->altitude = altitude;
+      this->v = v;
+      this->acc = acc;
+   }
+
+   private:
+      double altitude;
+      Acceleration acc;
+      Velocity v;
+   
+   public:
+      
+      double getAltitude()
+      {
+         return altitude;
+      }
+
+      void updateAltitude()
+      {
+         altitude = v.getDX() + (1 / 2) * acc.getDDX();
+      }
+};
