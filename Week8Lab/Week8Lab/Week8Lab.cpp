86836c27199a2458f26f8033cf10bdb459fde394
McClain Lindsay <mj.lindsay@live.com>
2025-02-22 07:35:09 -0800

    Lienear interpolation is created

---
+++ Week8Lab/Week8Lab/Week8Lab.cpp
@@ -0,0 +1,32 @@
+// Week8Lab.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+
+double calLienearInterpolationDistance(double r1, double r0, double d1, double d0, double r)
+{
+    return ((r1 - r0) / ((d1 - d0) * (r - r0))) + d0;
+}
+
+double calLienearInterpolationHeight(double r1, double r0, double d1, double d0, double d)
+{
+	return (r0 + (((r1 - r0) * (d - d0)) / (d1 - d0))); 
+}
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
