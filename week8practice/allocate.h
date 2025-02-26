3eadcacb49e1f6463c47a5b904747f6a5d50a7a3
EJ6612 <ej_6612@outlook.com>
2025-02-25 17:29:50 -0700

    Stud functions

--- week8practice/allocate.h
+++ week8practice/allocate.h
@@ -1,32 +1,55 @@
 /*************************************************************
  * 1. Name:
- *      -your name-
+ *      Elijah, McClain
+ * 
  * 2. Assignment Name:
  *      Practice 08: Allocation
+ * 
  * 3. Assignment Description:
  *      Allocate an array and then use it
+ * 
  * 4. What was the hardest part? Be as specific as possible.
  *      -a paragraph or two about how the assignment went for you-
+ * 
  * 5. How long did it take for you to complete the assignment?
  *      -total time in hours: reading the assignment, submitting, etc.
+ * 
  **************************************************************/
 
- /****************************
-  * ALLOCATE ONE FLOAT
-  ****************************/
 
+class Allocate
+{
+public:
+   /****************************
+   * ALLOCATE ONE FLOAT
+   ****************************/
+   void allocateOneFloat(float newFloat)
+   {
 
+   }
 
-/****************************
- * ALLOCATE ARRAY of DOUBLEs
- ****************************/
+   /****************************
+   * ALLOCATE ARRAY of DOUBLEs
+   ****************************/
+   void allocateArrayDouble(int slots)
+   {
 
+   }
 
-/****************************
- * DELETE ONE FLOAT
- ****************************/
+   /****************************
+   * DELETE ONE FLOAT
+   ****************************/
+   void deleteOneFloat(float* pFloat)
+   {
 
+   }
 
-/****************************
- * DELETE ARRAY of DOUBLEs
- ****************************/
+   /****************************
+   * DELETE ARRAY of DOUBLEs
+   ****************************/
+   void deleteArrayDouble()
+   {
+
+   }
+
+};
