09d1f215cc51a722060e35a655f81bfb0a345ee6
McClain Lindsay <mj.lindsay@live.com>
2025-03-04 09:06:57 -0800

    added files for week 9 practice

--- week8Practice/allocate.h
+++ week8Practice/allocate.h
@@ -1,32 +1,82 @@
 /*************************************************************
  * 1. Name:
- *      -your name-
+ *      Elijah, McClain
+ * 
  * 2. Assignment Name:
  *      Practice 08: Allocation
+ * 
  * 3. Assignment Description:
  *      Allocate an array and then use it
+ * 
  * 4. What was the hardest part? Be as specific as possible.
  *      -a paragraph or two about how the assignment went for you-
+ * 
  * 5. How long did it take for you to complete the assignment?
  *      -total time in hours: reading the assignment, submitting, etc.
+ * 
  **************************************************************/
 
- /****************************
-  * ALLOCATE ONE FLOAT
-  ****************************/
+class TestAllocate;
 
+class Allocate
+{
+   friend TestAllocate;
 
+public:
+   /****************************
+   * ALLOCATE ONE FLOAT
+   ****************************/
+   float * allocateOneFloat(float newFloat)
+   {
+      pFloat = new float(newFloat);
 
-/****************************
- * ALLOCATE ARRAY of DOUBLEs
- ****************************/
+      if (pFloat == nullptr)
+      {
+         std::cout << "ERROR";
+         return nullptr;
+      }
 
+      else
+      {
+         std::cout << "FLOAT ALLOCATED";
+         return pFloat;
+      }
 
-/****************************
- * DELETE ONE FLOAT
- ****************************/
+   }
 
+   /****************************
+   * ALLOCATE ARRAY of DOUBLEs
+   ****************************/
+   double * allocateArrayDouble(int slots)
+   {
+      if (slots <= 0) { return nullptr; }
 
-/****************************
- * DELETE ARRAY of DOUBLEs
- ****************************/
+      else
+      {
+         pSlots = new(nothrow) double pSlots [slots + 1];
+      }
+         
+   }
+
+   /****************************
+   * DELETE ONE FLOAT
+   ****************************/
+   void deleteOneFloat(float* pFloat)
+   {
+      delete pFloat;
+      std::cout << "FLOAT DELETED";
+   }
+
+   /****************************
+   * DELETE ARRAY of DOUBLEs
+   ****************************/
+   void deleteArrayDouble(double * p)
+   {
+      delete [] p;
+   }
+
+   private:
+      float* pFloat;
+      double* pSlots;
+
+};
