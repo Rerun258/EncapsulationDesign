c599cf56a51fda17f539b929b39dbd2c95a022c9
McClain Lindsay <mj.lindsay@live.com>
2025-03-04 09:57:01 -0800

    week9Practice done

--- week9practice/pointer.h
+++ week9practice/pointer.h
@@ -6,33 +6,48 @@
  * 3. Assignment Description:
  *      Traverse a string using pointer notation
  * 4. What was the hardest part? Be as specific as possible.
- *      -a paragraph or two about how the assignment went for you-
+ *     Understanding the instructions. 
  * 5. How long did it take for you to complete the assignment?
- *      -total time in hours: reading the assignment, submitting, etc.
+ *      30 minutes
  **************************************************************/
 
+using namespace std;
 
 /**************************************
  * COUNT - ARRAY
  **************************************/
 int countArray(char* cString, char single)
 {
-   int* pEnd =  + ;
-   for (int* p = array; p < pEnd; p++)
-      cout << *p << endl;
+	// Find the length of the string
+   int length = strlen(cString); 
+
+	int count = 0;
+
+   // If the indexes match, count it
+   for (int i = 0; i < length; i++)
+   {
+      if (cString[i] == single)
+         count++;
+   }
+
+	return count;
 }
+
+
 
 /**************************************
  * COUNT - POINTER
  **************************************/
 int countPointer(char* cString, char single)
 {
-   int count = 0;
+	int count = 0; 
+
    for (char * p = cString; *p; p++)
    {
+      // If the characters match, count it
       if (*p == single)
-         return 0;
+			count++;
    }
 
-   return 0;
+   return count;
 }
