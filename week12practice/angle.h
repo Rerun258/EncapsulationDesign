1b03f387d08506a330db301cf7c411aa5c81d637
EJ6612 <ej_6612@outlook.com>
2025-03-25 14:53:03 -0600

    Week 04 brought over

--- week12practice/angle.h
+++ week12practice/angle.h
@@ -1,6 +1,6 @@
 /*************************************************************
  * 1. Name:
- *      -your name-
+ *      Elijah, McClain
  * 2. Module
  *      ANGLE
  * 3. Assignment Description:
@@ -27,33 +27,84 @@ class Angle
 {
    friend TestAngle;
 
-   // Angle, stored in radians.
-   double radians;
+   // Constructors
+   Angle() : radians(0.0) {}
 
-public:
+   Angle(const Angle& rhs) : radians(rhs.getRadians()) {}
 
-   double getDegrees()
+   Angle(double degrees)
    {
-      double degrees = radians * (180 / M_PI);
+      setDegrees(degrees);
+   }
+
+   Angle& add(double delta)
+   {
+      radians += delta;
+
+      // Normalize the angle after addition
+      radians = normalize(radians);
+
+      return *this;
+   }
+
+   // Getters
+   double getDegrees() const { return radians * (180.0 / M_PI); }
+   double getRadians() const { return radians; }
+
+
+   // Setters
+   void setDegrees(double degrees)
+   {
+      degrees = (degrees * (2.0 * M_PI)) / 360.0;
+      radians = normalize(degrees);
+   }
+
+   void setRadians(double aRadian) { radians = normalize(aRadian); }
+
+
+   // Converters
+   double convertToDegrees(double aRadian) const
+   {
+      double degrees = round(normalize(aRadian) * (180.0 / M_PI));
       return degrees;
    }
 
-   double getRadians()
+   double convertToRadians(double aDegree) const
    {
-      return radians;
+      double radians = aDegree * (M_PI / 180.0);
+      return normalize(radians);
    }
 
-   void setDegrees(double aDegree)
+   void setUp()
    {
-      radians = normalize(convertToRadians(aDegree));
+      // 0°
+      radians = 0.0;
    }
 
-   void setRadians(double aRadian)
+   void setRight()
    {
-      radians = normalize(aRadian);
+      // 90°
+      radians = M_PI * 0.5;
    }
 
-   void display(ostream& out)
+   void setLeft()
+   {
+      // 270°
+      radians = M_PI * 1.5;
+   }
+
+   void setDown()
+   {
+      // 180°
+      radians = M_PI;
+   }
+
+   void reverse()
+   {
+      radians += M_PI;
+   }
+
+   void display(ostream& out) const
    {
       out.setf(ios::fixed);     // "fixed" means don't use scientific notation.
       out.setf(ios::showpoint); // "showpoint" means always show the decimal point.
@@ -62,33 +113,28 @@ public:
       out << getDegrees() << "degrees";
    }
 
+
 private:
+   double normalize(double aRadian) const;
 
-   double normalize(double aRadian)
-   {
-      while (aRadian > TWO_PI)
-      {
-         aRadian -= TWO_PI;
-      }
-
-      while (aRadian < 0)
-      {
-         aRadian += TWO_PI;
-      }
-
-      return aRadian;
-   }
-
-   double convertToDegrees(double aRadian)
-   {
-      double degrees = round(normalize(aRadian) * (180 / M_PI));
-      return degrees;
-   }
-
-   double convertToRadians(double aDegree)
-   {
-      double radians = aDegree * (M_PI / 180);
-      return normalize(radians);
-   }
-
+   double radians;
+   // 360 degrees equals 2 PI radians
 };
+
+
+double Angle::normalize(double aRadian) const
+{
+   // Use fmod to find the remainder of aRadian divided by TWO_PI
+   aRadian = fmod(aRadian, TWO_PI);
+
+   if (aRadian < 0)
+   {
+      aRadian += TWO_PI;
+   }
+   if (aRadian > TWO_PI)
+   {
+      aRadian -= TWO_PI;
+   }
+
+   return aRadian;
+}
