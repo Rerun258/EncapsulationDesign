55ec04e519e0451254a907c5c282e1baccf21784
EJ6612 <ej_6612@outlook.com>
2025-04-05 11:17:58 -0600

    sharing bugs

--- week12Lab/projectile.h
+++ week12Lab/projectile.h
@@ -38,38 +38,7 @@ public:
    Projectile() : mass(DEFAULT_PROJECTILE_WEIGHT), radius(DEFAULT_PROJECTILE_RADIUS) {}
 
    // Advance the round forward until the next unit of time.
-   void advance(double simulationTime)
-   {
-      if (!flying())
-         return;
-
-      PositionVelocityTime pvt = flightPath.back();
-      double speed = pvt.v.getSpeed();
-      double altititude = pvt.pos.getMetersY();
-      double interval = simulationTime - currentTime();
-      assert(interval > 0.0);
-
-      double density = densityFromAltitude(altititude);
-      double speedSound = speedSoundFromAltitude(altititude);
-      double mach = speed / speedSound;
-      double dragCoefficient = dragFromMach(mach);
-      double windResistence = forceFromDrag(density, dragCoefficient, radius, speed);
-      double magnitudeWind = accelerationFromForce(windResistence, mass);
-      Acceleration aWind(pvt.v.getAngle(), magnitudeWind);
-
-      double magnitudeGravity = gravityFromAltitude(altititude);
-      Angle angleGravity;
-      angleGravity.setDown();
-      Acceleration aGravity(angleGravity, magnitudeGravity);
-
-      Acceleration aTotal = aGravity + aWind;
-
-      pvt.pos.add(aTotal, pvt.v, interval);
-      pvt.v.add(aTotal, interval);
-      pvt.t = simulationTime;
-
-      flightPath.push_back(pvt);
-   }
+   void advance(double simulationTime);
 
    void fire(const Position& pos, double simulationTime, const Angle& elevation, double muzzleVelocity)
    {
